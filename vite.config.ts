bda6cbe9afc96807e3d143987f8f54b019f31409
momei <momei.lu@yunlsp.com>
2025-01-28 13:44:37 +0800

    feat: build

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,27 @@
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-import UnoCSS from 'unocss/vite'
-import { defineConfig } from 'vite'
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import UnoCSS from "unocss/vite";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-    build: {
-        lib: {
-            name: 'momei',
-            entry: ['./src/components/index.ts'],
-            fileName: (format, entryName) => `momei-${entryName}.${format}.js`,
-            cssFileName: 'momei-style',
+  build: {
+    rollupOptions: {
+      external: ["vue"],
+      output: {
+        globals: {
+          vue: "Vue",
         },
-        outDir: 'dist',
-        minify: false,
+      },
     },
+    lib: {
+      name: "momei",
+      entry: ["./src/components/index.ts"],
+      fileName: (format, entryName) => `momei-${entryName}.${format}.js`,
+      cssFileName: "momei-style",
+    },
+    outDir: "dist",
+    minify: false,
+  },
 
-    plugins: [vue(), vueJsx({}), UnoCSS()],
-})
+  plugins: [vue(), vueJsx({}), UnoCSS()],
+});
